8667a9daaf589b0c8aa7ac23dde5fd25b86b9ffc
Radosław Szamszur <github@rsd.sh>
2022-10-08 16:41:34 +0200

    Add generate.sh

---
+++ generate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+copier -d project_name=example \
+  -d container_image_name=example \
+  -d chart_name=example \
+  -d script_name=example \
+  -d project_description="This project was generated with fastapi-mvc." \
+  -d author="Radosław Szamszur" \
+  -d email=github@rsd.sh \
+  -d copyright_date=2022 \
+  -d nix=true \
+  -d redis=true \
+  -d aiohttp=true \
+  -d github_actions=true \
+  -d helm=true \
+  -d license=mit \
+  -d repo_url=https://github.com/fastapi-mvc/example \
+  -d version=0.1.0 \
+  -d fastapi_mvc_version="Generated from CI" \
+  copy . example

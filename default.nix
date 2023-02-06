83407dc91b47ee79d7aa768b1dcd1f4ec204aa7d
Radosław Szamszur <github@rsd.sh>
2023-02-06 19:19:45 +0100

    Extend poetry2nix overrides

--- default.nix
+++ default.nix
@@ -6,4 +6,11 @@ poetry2nix.mkPoetryEnv {
   inherit python;
   pyproject = ./pyproject.toml;
   poetrylock = ./poetry.lock;
+  overrides = poetry2nix.overrides.withDefaults (final: prev: {
+    pyyaml-include = prev.pyyaml-include.overridePythonAttrs (old: {
+      postPatch = ''
+        substituteInPlace setup.py --replace 'setup()' 'setup(version="${old.version}")'
+      '';
+    });
+  });
 }
